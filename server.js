7ca5107f8957388768a9419ee936b32932f787ac
catchbb14 <ericmorrow14@gmail.com>
2018-06-26 16:37:50 -0400

    Removed morgan from application. For now...

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
-var logger = require("morgan");
+// var logger = require("morgan");
 var mongoose = require("mongoose");
+
 
 var PORT = 3000;
 
 // Require all models
 var db = require("./models");
 
-// Initialize Express
 var app = express();
 
 // Configure middleware
 
 // Use morgan logger for logging requests
-app.use(logger("dev"));
+// app.use(logger("dev"));
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
+
+//Setting up handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+}));
+
+app.set('view engine', 'handlebars');
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/mongoHeadlines");
@@ -30,6 +38,13 @@ db.User.create({ name: "Admin" })
   .catch(function(err) {
     console.log(err.message);
 });
+
+//Routes
+// require('./routes/api-routes.js')(app);
+// require('./routes/html-routes.js')(app);
+app.get('/', function(req, res) {
+    res.send("It worked I think!")
+})
 
 // Start the server
 app.listen(PORT, function() {
