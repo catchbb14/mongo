9d77500e9a863d90a1a2ab56533de94656ebad3e
Eric Morrow <ericmorrow14@gmail.com>
2018-07-15 14:17:20 -0400

    Added click functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,56 @@
 
+$(document).ready(function() {
+    //Save Event
 
+    $(document).on("click", ".saveEvent", function(event) {
 
-$(document).on("click", ".saveEvent", function(event) {
+        event.preventDefault();
+    
+        var id = $(this).data('id');
+        $(this).remove();
+        $.ajax({
+            method: "PUT",
+            url: "/articles/" + id,
+            data: { saved: true }
+        }).done(function(res) {
+            console.log(res);
+        })
+    
+    });
 
-    event.preventDefault;
+    $(document).on('click', '.deleteSaved', function() {
+        var id = $(this).data('id');
+        $.ajax({
+            method: "PUT",
+            url: "/articles/" + id,
+            data: { saved: false }
+        }).done(function(res) {
+            console.log(res);
+            location.reload();
+        })
+        
+    });
 
-    var id = $(this).attr('data-id');
-    $.ajax({
-        method: "PUT",
-        url: "/articles/" + id,
-        data: { saved: true }
-    }).done(function(res) {
-        console.log(res);
+    $(document).on('click', '.addComment', function() {
+        var id = $(this).data('id');
+        var commentText = $(this).parent().parent().parent()
+            .children('.input-group').children('input');
+        console.log(commentText.val())
+
+        $.ajax({
+            method: "POST",
+            url: "/articles/" + id,
+            data: {
+               body: commentText.val()
+            }
+        }).done(function(res) {
+            console.log(res);
+            commentText.val("")
+            location.reload();
+        })
     })
 
+    // ["0"].parentElement.parentElement.parentElement.children[2]
 })
+
     
